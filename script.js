68ac458edaefaef6c25a4600f4b3268d82a93c00
Graciela Parodi <31286727+gracielaparodi@users.noreply.github.com>
2022-08-24 18:55:10 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/* FOR  */
+
+for (let i = 0; i < 31; i = i + 7) {
+    if (i === 21) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+/*  WHILE 
+Ingreso de usuario y contraseña*/
+
+
+let usuario = prompt("Ingrese el nombre de usuario");
+
+while (contraseña != "1234") {
+    var contraseña = prompt("Ingresa la Contraseña");
+}
+alert("Contraseña Correcta");
+
+
+
